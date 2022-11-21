d475f184551ed219846ef652b6770f11b9fdc0a1
djava387 <risad9020@gmail.com>
2022-11-21 20:09:56 +0000

    Updated files

---
+++ script.js
@@ -0,0 +1,15 @@
+// creating constants for elements
+const calcDis = document.querySelector("h1");
+// targetting  all button elements i.e returns array of all the buttons 
+const inputButton = document.querySelectorAll ("button");
+const clearButton =  document.getElementById ("clear-btn");
+//console logging  parameters to be passed from evenlistenners
+function inputNumberValue (number) {
+console.log(number);
+}
+
+// console.log(inputButton);
+//creating eventlistenners for all the buttons
+
+
+
